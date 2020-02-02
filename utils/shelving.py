7fbcd2eb69dee9f1dccb9e546f1438e9a6e77771
Janis Lesinskis <janis@scrygroup.com>
2020-02-02 23:37:57 +1100

    Added repr

--- utils/shelving.py
+++ utils/shelving.py
@@ -7,3 +7,10 @@ class Shelving:
         self.width = width
         self.depth = depth
         self.max_weight = max_weight
+        self.label = label
+
+    def __repr__(self) -> str:
+        if self.max_weight:
+            return f"Shelving(label={self.label}, width={self.width}, depth={self.depth}, max_weight={self.max_weight})"
+        else:
+            return f"Shelving(label={self.label}, width={self.width}, depth={self.depth})"
