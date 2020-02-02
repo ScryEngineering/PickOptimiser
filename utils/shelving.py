40e6f18f888fd57ba6d4821c9330d1f414d1712d
Janis Lesinskis <janis@scrygroup.com>
2020-02-02 23:13:04 +1100

    More implementation of shelving class

--- utils/shelving.py
+++ utils/shelving.py
@@ -1,5 +1,9 @@
 class Shelving:
-    def __init__(self, *, max_weight):
-        """Represent shelving
-        max_weight: the maximum weight a shelve can take"""
-        raise NotImplementedError
+    def __init__(self, *, width, depth, max_weight=None):
+        """Represent rectangular shelving
+        width: the length from which items are accessible
+        depth: the length that represents how far the shelving goes back.
+        max_weight: the maximum weight a shelve can take as load"""
+        self.width = width
+        self.depth = depth
+        self.max_weight = max_weight
